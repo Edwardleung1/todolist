519619b51e259bce0deda83351f30e6ed7e5580d
Edward Leung <edward_leung_12@hotmail.com>
2020-07-09 18:33:59 +0100

    update

--- app.js
+++ app.js
@@ -144,5 +144,8 @@ function removeLocalTodos(todo) {
     } else {
         todos = JSON.parse(localStorage.getItem('todos'));
     }
-    
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
+
